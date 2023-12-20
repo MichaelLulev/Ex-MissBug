d4d3d87ba8e6105f337c13063485ab2467f34e5d
Michael Lulev <miki1990@gmail.com>
2023-12-20 12:16:48 +0200

    Improve styling

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -6,6 +6,6 @@ export function BugPreview({bug}) {
         <h4>{bug.title}</h4>
         <h1>🐛</h1>
         <p>Severity: <span>{bug.severity}</span></p>
-        <p>Description: <span><LongText text={bug.description} length={40}/></span></p>
+        <p>Description: <span><LongText text={bug.description} length={50}/></span></p>
     </article>
 }
--- public/cmps/LongText.jsx
+++ public/cmps/LongText.jsx
@@ -4,7 +4,8 @@ export function LongText(props) {
     const [isLong, setIsLong] = useState(false)
 
     const text = props.text
-    const length = props.length || 100
+    const length = props.length
+    const isExpandable = props.expandable
 
     function textToShow() {
         if (text.length <= length || isLong) return text
@@ -14,14 +15,15 @@ export function LongText(props) {
     function toggleIsLong() {
         setIsLong(prevIsLong => ! prevIsLong)
     }
-
+    
+    if (! text || ! length) return <span></span>
     return (
         <span>
             <span>
                 {textToShow()}
             </span>
         {
-            length < text.length &&
+            isExpandable && length < text.length &&
             <button onClick={toggleIsLong}>
                 {isLong ? 'Less' : 'More'}
             </button>
